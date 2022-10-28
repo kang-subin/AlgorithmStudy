544735e1b14b063934ccaf40dd77174ef78263a7
강수빈 <tnbin98@gmail.com>
2022-10-28 10:02:31 +0900

    백준: 폰켓몬

---
+++ src/main/java/week6/Pokemon.java
@@ -0,0 +1,26 @@
+package week6;
+import java.util.HashSet;
+
+//포켓몬 종류 배열 있다고 가정
+// 포켓몬 종류 배열의 길이 나누기 2 한 갯수를 가져갈 수 있음
+// 포켓몬 종류의 중복 제거한 최대의 수 구하면 됨?
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pokemon {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length ; i++) {
+            set.add(nums[i]);
+        }
+        int N = nums.length / 2;
+        if (set.size() < N) {
+            return set.size();
+        }else{
+            return N;
+        }
+    }
+
+}
+
