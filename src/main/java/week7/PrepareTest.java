482c8983b00e34ac84420c28ebfc78de28f2398b
강수빈 <tnbin98@gmail.com>
2022-10-31 11:02:36 +0900

    백준: 모의고사 푸는중...

--- src/main/java/week7/PrepareTest.java
+++ src/main/java/week7/PrepareTest.java
@@ -6,20 +6,23 @@ import java.util.List;
 
 public class PrepareTest {
     int[] solution(int[] answer) {
-        int[] first = {1, 2, 3, 4, 5};
-        int[] score = new int[5];
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = {0, 0, 0};
         for (int i = 0; i < answer.length; i++) {
-            if (first[i] == answer[i]) {
-                score[i] = answer[i];
+            if (arr1[i] == answer[i]) {
+                int count = ++i;
+                score[0] = count;
             }
         }
         return score;
     }
 
     public static void main(String[] args) {
-PrepareTest prepareTest = new PrepareTest();
-    int [] answer = {1,2,3,4,5};
-   int answerScore [] = prepareTest.solution(answer);
-
+        PrepareTest prepareTest = new PrepareTest();
+        int[] answer = {1, 2, 3, 4, 5};
+        int count[] = prepareTest.solution(answer);
+        System.out.println(count[0]);
     }
 }
