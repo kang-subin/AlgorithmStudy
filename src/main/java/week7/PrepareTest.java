3fb0b432c2380987e08e91790cac2a250bf352fb
강수빈 <tnbin98@gmail.com>
2022-10-31 10:36:03 +0900

    백준: 모의고사 푸는중...

--- src/main/java/week7/PrepareTest.java
+++ src/main/java/week7/PrepareTest.java
@@ -1,15 +1,25 @@
 package week7;
 
-public class PrepareTest
-{
-int [] solution(int [] answers) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
-    return new int[0];
+public class PrepareTest {
+    int[] solution(int[] answer) {
+        int[] first = {1, 2, 3, 4, 5};
+        int[] score = new int[5];
+        for (int i = 0; i < answer.length; i++) {
+            if (first[i] == answer[i]) {
+                score[i] = answer[i];
+            }
+        }
+        return score;
     }
 
     public static void main(String[] args) {
+PrepareTest prepareTest = new PrepareTest();
+    int [] answer = {1,2,3,4,5};
+   int answerScore [] = prepareTest.solution(answer);
 
     }
-
 }
