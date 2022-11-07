d5fabf3485c0ab1df8fae553326a6bd96c784d23
강수빈 <tnbin98@gmail.com>
2022-11-07 10:35:55 +0900

    백준: 소수 찾기 (나누어 떨어지는 숫자 배열)

--- src/main/java/week8/Numberarr.java
+++ src/main/java/week8/Numberarr.java
@@ -3,6 +3,7 @@ package week8;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Numberarr {
     int[] solution(int[] arr, int divisor) {
@@ -17,6 +18,24 @@ public class Numberarr {
             answer[i] = list.get(i);
         }
         Arrays.sort((answer));
+        return answer;
+    }
+
+    public int[] solution2(int[] arr, int divisor) {
+
+        PriorityQueue<Integer> list = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) list.add(arr[i]);
+        }
+
+        if(list.size() == 0) return new int[]{-1};
+
+        // list를 Array로 바꾸고
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        while(!list.isEmpty()){
+            answer[idx++] = list.poll();
+        }
         return answer;
     }
 
