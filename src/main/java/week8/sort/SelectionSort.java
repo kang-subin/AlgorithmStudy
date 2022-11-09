79958ed437600b870df888efa5a227fc751c5aa3
강수빈 <tnbin98@gmail.com>
2022-11-09 10:31:54 +0900

    람다식 적용

--- src/main/java/week8/sort/SelectionSort.java
+++ src/main/java/week8/sort/SelectionSort.java
@@ -3,13 +3,30 @@ package week8.sort;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+    public int[] SelectionSortAsc(int[] arr , StatementStrategy statementStrategy) {
 
         for (int i = 0; i < arr.length - 1; i++) {
-            int maxIdx = arr.length -1;
+            int minIdx = i;
             for (int j = i; j < arr.length; j++) {
-                if(arr[maxIdx] < arr[j]) maxIdx = j;
+                if (arr[minIdx] > arr[j]) minIdx = j;
+            } // 크기 비교
+
+            //위치 변경
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+            System.out.println(Arrays.toString(arr));
+
+        }
+                return arr;
+    }
+
+    public int[] selectionSrotDesc (int[] arr, StatementStrategy statementStrategy) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int maxIdx = arr.length - 1;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[maxIdx] < arr[j]) maxIdx = j;
             } // 크기 비교
 
             //위치 변경
@@ -17,8 +34,18 @@ public class SelectionSort {
             arr[i] = arr[maxIdx];
             arr[maxIdx] = temp;
             System.out.println(Arrays.toString(arr));
-
         }
+          return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort ss = new SelectionSort();
+        ss.SelectionSortAsc(arr, (a, b) -> a > b);
+        ss.selectionSrotDesc(arr, (a, b) -> a < b);
+
+
+
     }
 }
 
