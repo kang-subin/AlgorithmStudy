f8c75824a5ef0839ea1c46c557300be61436a0ef
강수빈 <tnbin98@gmail.com>
2022-11-09 10:11:41 +0900

    내림차순 적용

--- src/main/java/week8/sort/SelectionSort.java
+++ src/main/java/week8/sort/SelectionSort.java
@@ -7,15 +7,15 @@ public class SelectionSort {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
         for (int i = 0; i < arr.length - 1; i++) {
-            int minIdx = i;
+            int maxIdx = arr.length -1;
             for (int j = i; j < arr.length; j++) {
-                if(arr[minIdx] > arr[j]) minIdx = j;
+                if(arr[maxIdx] < arr[j]) maxIdx = j;
             } // 크기 비교
 
             //위치 변경
             int temp = arr[i];
-            arr[i] = arr[minIdx];
-            arr[minIdx] = temp;
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
             System.out.println(Arrays.toString(arr));
 
         }
