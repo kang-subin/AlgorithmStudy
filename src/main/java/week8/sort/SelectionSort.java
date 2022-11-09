44ac45471b91993ba332a653870f7966e44f2341
강수빈 <tnbin98@gmail.com>
2022-11-09 10:08:42 +0900

    선택정렬 (로직완성)

--- src/main/java/week8/sort/SelectionSort.java
+++ src/main/java/week8/sort/SelectionSort.java
@@ -6,67 +6,23 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
-        int i = 0;
-        int minIdx = 0; //
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[minIdx] > arr[j]) minIdx = j;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if(arr[minIdx] > arr[j]) minIdx = j;
+            } // 크기 비교
+
+            //위치 변경
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+            System.out.println(Arrays.toString(arr));
+
         }
-
-        int temp = arr[0];
-        arr[0] = arr[minIdx];
-        arr[minIdx] = temp;
-        System.out.println(Arrays.toString(arr));
-
-        i = 1;
-        minIdx = 1;
-        for (int j = 1; j < arr.length; j++) {
-            if (arr[minIdx] > arr[j]) minIdx = j;
-        }
-        temp = arr[1];
-        arr[1] = arr[minIdx];
-        arr[minIdx] = temp;
-        System.out.println(Arrays.toString(arr));
-
-        i = 2;
-        minIdx = 2;
-        for (int j = 2; j < arr.length; j++) {
-            if (arr[minIdx] > arr[j]) minIdx = j;
-        }
-        temp = arr[2];
-        arr[2] = arr[minIdx];
-        arr[minIdx] = temp;
-        System.out.println(Arrays.toString(arr));
-
-        i = 3;
-        minIdx = 3;
-        for (int j = 3; j < arr.length; j++) {
-            if (arr[minIdx] > arr[j]) minIdx = j;
-        }
-        temp = arr[3];
-        arr[3] = arr[minIdx];
-        arr[minIdx] = temp;
-        System.out.println(Arrays.toString(arr));
-
-        i = 4;
-        minIdx = 4;
-        for (int j = 4; j < arr.length; j++) {
-            if (arr[minIdx] > arr[j]) minIdx = j;
-        }
-        temp = arr[4];
-        arr[4] = arr[minIdx];
-        arr[minIdx] = temp;
-        System.out.println(Arrays.toString(arr));
-
-
-
-
-
     }
+}
 
 
-
-
-    }
 
 
 
