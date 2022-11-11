31d47ae77212f405f4f66304e5e84202ee480896
강수빈 <tnbin98@gmail.com>
2022-11-11 10:47:17 +0900

    프로그래머스 비밀지도 (비트연산적용)

--- src/main/java/week8/secretMap.java
+++ src/main/java/week8/secretMap.java
@@ -1,5 +1,7 @@
 package week8;
 
+import java.util.Arrays;
+
 public class secretMap {
     //비밀지도 로직
     // 배열 arr1, arr2 를 이진수로 변환한다
@@ -26,10 +28,49 @@ String getBinaryString2 (int n){
 
 }
 
+String solution (int n, int[] arr1, int[] arr2){
+        String answer ="";
+        String [] sArr1 = new String[n];
+        String [] sArr2 = new String[n];
+    for (int i = 0; i < n ; i++) {
+        for (int j = 0; j < n; j++) {
+            String binStr1 = getBinaryString(arr1[i]);
+            String binStr2 = getBinaryString(arr2[i]);
+        sArr1[i] = "0".repeat(n - binStr1.length()) + binStr1 ; // arr1[i]에 0 을 n-(이진수자릿수) 만큼 더한 후 배열에 넣기
+        sArr2[i] = "0".repeat(n - binStr2.length()) + getBinaryString(arr2[i]);
+        }
+    }
+        System.out.println(Arrays.toString(sArr1));
+        System.out.println(Arrays.toString(sArr2));
+
+        return answer;
+    }
+
+    String[] solution2(int n, int[] arr1, int[] arr2) {
+        var answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]) // | 비트연산을 통해서 arr1[i], arr2[i] 를 계산한 이진수 값을 도출
+                    .replace("1", "#").replace("0", " "); //#," " 형태로 변환
+            answer[i] = " ".repeat(n - answer[i].length()) + answer[i]; // 자릿수 맞추기
+        }
+
+        System.out.println(Arrays.toString(answer));
+
+        return answer;
+    }
 
 
 
 
+    public static void main(String[] args) {
+        int [] arr1 = {9, 20, 28, 18, 11};
+        int [] arr2 ={30, 1, 21, 17, 28};
+        secretMap SecretMap = new secretMap();
+        SecretMap.solution(5 ,arr1,arr2);
+        SecretMap.solution2(5,arr1, arr2);
+
+
+    }
 
 
 
