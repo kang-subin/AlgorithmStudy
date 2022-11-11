cfe85099780161e604de669be8ebe6a6efa13443
강수빈 <tnbin98@gmail.com>
2022-11-11 09:45:59 +0900

    이진수로 변환

--- src/main/java/week8/secretMap.java
+++ src/main/java/week8/secretMap.java
@@ -5,7 +5,31 @@ public class secretMap {
     // 배열 arr1, arr2 를 이진수로 변환한다
     // arr1, arr2의 중복을 허용하고 합친다..?
 
-    //이진수 로직...
+    //이진수 변환
+    String getBinaryString(int decNum) {
+        String answer = "";
+        while (decNum > 0) {
+            answer += decNum % 2;
+            decNum /= 2;
+        }
+        return answer;
+    }
+
+
+String getBinaryString2 (int n){
+        String answer ="";
+    while (n > 0) {
+        answer += n % 2;
+        n = n/2;
+    }
+        return answer;
+
+}
+
+
+
+
+
 
 
 
