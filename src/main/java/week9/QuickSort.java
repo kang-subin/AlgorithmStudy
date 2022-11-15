9ddaacd674c28f6e9a1f424a6e76e558df196d47
강수빈 <tnbin98@gmail.com>
2022-11-15 09:21:28 +0900

    퀵 정렬 : 기준값 뽑는 로직 구현

---
+++ src/main/java/week9/QuickSort.java
@@ -0,0 +1,16 @@
+package week9;
+
+public class QuickSort {
+
+    public int solution (int [] arr){
+            int mid = arr.length / 2 ;
+            int  Number= arr[mid];
+        return Number;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20, 18, 5, 19, 5, 25, 40, 50};
+
+
+    }
+}
