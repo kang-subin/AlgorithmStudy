7b423a5f45f205dfe1577af07c80bd450339ac4f
강수빈 <tnbin98@gmail.com>
2022-11-22 09:13:28 +0900

    재귀호출

---
+++ src/main/java/week10/Print1To100V2.java
@@ -0,0 +1,14 @@
+package week10;
+
+public class Print1To100V2 {
+    public static void print1To100(int n) {
+        System.out.println(n);
+        print1To100(n);
+    }
+
+    public static void main(String[] args) {
+        print1To100(1);
+    }
+
+
+}
