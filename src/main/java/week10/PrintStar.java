7a0457b26073957d3d63a07ba18d10403c8b54a8
강수빈 <tnbin98@gmail.com>
2022-11-24 10:35:21 +0900

    codeup 1859 : 별 삼각형 출력하기

--- src/main/java/week10/PrintStar.java
+++ src/main/java/week10/PrintStar.java
@@ -8,7 +8,7 @@ public static String star (int n){
     return star + star(n-1) ;
 }
 
-public static void printStar(int n){
+public static void printStar(int n){ // stack 메모리는 후입선출 구조라서 내가 처음 출력한 것이 맨 마지막에 호출된다
     if(n==0) return;
     printStar(n-1);
     System.out.println(star(n));
